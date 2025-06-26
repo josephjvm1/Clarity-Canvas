f239a3c9eecc54096d4daa2735e34244e6007f3d
Joseph Mairena <joseph@palmtreeproperties.com>
2025-06-26 10:08:13 -0700

    Updated Auth.jsx with Google login config

---
+++ clarity-canvas/src/Auth.jsx
@@ -0,0 +1,17 @@
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { supabase } from './supabaseClient';
+
+function AuthComponent() {
+  return (
+    <div className="auth-container">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={['google']}
+      />
+    </div>
+  );
+}
+
+export default AuthComponent;
